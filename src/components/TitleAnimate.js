10fd9e39cb7cd73a8194d1c672fba75e36a94500
DoubleHelix <tjrnyc5@gmail.com>
2020-12-29 16:21:54 -0500

    fixed width styling due to browser zoom errors

--- src/components/TitleAnimate.js
+++ src/components/TitleAnimate.js
@@ -10,40 +10,40 @@ export default function TitleAnimate () {
     console.log('hello', title);
     if (title.length>0)
     { setTimeout(() => { 
-            title[0].style["font-size"]='18px'; 
+            title[0].style["font-size"]='28px'; 
             title[0].style["color"]='rgb(91, 228, 240)'; 
             title[0].style["font-family"]='game-show2'; 
 
             // questionMarks[0].textContent='¿';
-            questionMarks[0].style["font-size"]='18px'; 
+            questionMarks[0].style["font-size"]='28px'; 
             questionMarks[0].style["color"]='rgb(91, 228, 240)'; 
             questionMarks[0].style["font-family"]='game-show'; 
         }, 250);
 
         setTimeout(() => { 
-            title[1].style["font-size"]='18px'; 
+            title[1].style["font-size"]='28px'; 
             title[1].style["color"]='rgb(252, 107, 87)'; 
             title[1].style["font-family"]='game-show2'; 
 
             // questionMarks[1].textContent='¿';
-            questionMarks[1].style["font-size"]='18px'; 
+            questionMarks[1].style["font-size"]='28px'; 
             questionMarks[1].style["color"]='rgb(252, 107, 87)'; 
             questionMarks[1].style["font-family"]='game-show'; 
         }, 500);
         
         setTimeout(() => { 
-        title[2].style["font-size"]='18px'; 
+        title[2].style["font-size"]='28px'; 
         title[2].style["color"]='rgb(126, 80, 168)'; 
         title[2].style["font-family"]='game-show2'; 
 
         // questionMarks[2].textContent='¿';
         questionMarks[2].style["color"]='rgb(126, 80, 168)'; 
         questionMarks[2].style["font-family"]='game-show'; 
-        questionMarks[2].style["font-size"]='18px'; 
+        questionMarks[2].style["font-size"]='28px'; 
         }, 750);
         
         setTimeout(() => { 
-        title[3].style["font-size"]='18px'; 
+        title[3].style["font-size"]='28px'; 
         title[3].style["color"]='rgb(126, 80, 168)'; 
         title[3].style["font-family"]='game-show';
 
