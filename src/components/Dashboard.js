4eda502979bb8682caa0a56ff22464b57afdfedb
DoubleHelix <tjrnyc5@gmail.com>
2020-12-31 19:54:35 -0500

    finished questions tabs for now

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,30 +7,33 @@ export default function dashboard () {
 
     e.preventDefault();
     if (e.target.className === 'unanswered') { 
-      unanswered[0].style.color='rgb(255, 255, 255)';
-      unanswered[0].style['text-shadow']='2px 2px 0 #000000, 2px -2px 0 #000000, -2px 2px 0 #000000, -2px -2px 0 #000000, 2px 0px 0 #000000, 0px 2px 0 #000000, -2px 0px 0 #000000, 0px -2px 0 #000000'
-      answered[0].style.color='black';
-      answered[0].style['text-shadow']='none';
+      unanswered[0].style.color='black';
+      unanswered[0].style['box-shadow']='0px 30px 0px -10px rgb(255, 255, 255), 0px 0px 0px 0px #FFFFFF, 0px 5px 10px -10px #ffffff, 0px 5px 10px -10px #FFFFFF,  0px 2px  #020405, 2px 2px 4px 2px rgba(255, 5, 5, 0)';
+      answered[0].style.color='#b9b9b9';
+      answered[0].style['box-shadow']='0px 30px 0px -10px rgb(255, 255, 255), 0px 0px 0px 0px #FFFFFF, 0px 5px 10px -10px #ffffff, 0px 5px 10px -10px #FFFFFF,  0px 2px  #f3f3f3, 2px 2px 4px 2px rgba(255, 5, 5, 0)';
+      
     }
     else if (e.target.className === 'answered') {
-      answered[0].style.color='rgb(255, 255, 255)';
-      answered[0].style['text-shadow']='2px 2px 0 #000000, 2px -2px 0 #000000, -2px 2px 0 #000000, -2px -2px 0 #000000, 2px 0px 0 #000000, 0px 2px 0 #000000, -2px 0px 0 #000000, 0px -2px 0 #000000'
-      unanswered[0].style.color='black';
-      unanswered[0].style['text-shadow']='none';
+      answered[0].style.color='black';
+      answered[0].style['box-shadow']='0px 30px 0px -10px rgb(255, 255, 255), 0px 0px 0px 0px #FFFFFF, 0px 5px 10px -10px #ffffff, 0px 5px 10px -10px #FFFFFF,  0px 2px  #020405, 2px 2px 4px 2px rgba(255, 5, 5, 0)';
+      unanswered[0].style.color='#b9b9b9';
+      unanswered[0].style['box-shadow']='0px 30px 0px -10px rgb(255, 255, 255), 0px 0px 0px 0px #FFFFFF, 0px 5px 10px -10px #ffffff, 0px 5px 10px -10px #FFFFFF,  0px 2px  #f3f3f3, 2px 2px 4px 2px rgba(255, 5, 5, 0)';
+      
     }
 
   }
+  
 
 
   return (
     
     <div className='question-table'>
         <div onClick={(event) => QTabChange(event)} className='unanswered'>
-          Unanswered
+          Unanswered Questions
         </div>
 
         <div onClick={(event) => QTabChange(event)} className='answered'>
-            Answered
+            Answered Questions
         </div>
 
         <div className='questions'>
