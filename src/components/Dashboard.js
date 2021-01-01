d01c569b2af01395e1fbdf18f1cbea376118340c
DoubleHelix <tjrnyc5@gmail.com>
2020-12-31 22:09:31 -0500

    finished question card who div css

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,33 +1,43 @@
+import * as DataAPI from '../utils/_DATA'
+
+
 
 export default function dashboard () {
+
   let unanswered= document.getElementsByClassName('unanswered');
   let answered= document.getElementsByClassName('answered');
+  let questions = DataAPI.questions['8xf0y6ziyjabvozdd253nd'];
+  let users = DataAPI.users['sarahedo'];
 
+ 
   function QTabChange(e){
 
     e.preventDefault();
     if (e.target.className === 'unanswered') { 
       unanswered[0].style.color='black';
-      unanswered[0].style['box-shadow']='0px 30px 0px -10px rgb(255, 255, 255), 0px 0px 0px 0px #FFFFFF, 0px 5px 10px -10px #ffffff, 0px 5px 10px -10px #FFFFFF,  0px 2px  #020405, 2px 2px 4px 2px rgba(255, 5, 5, 0)';
+      unanswered[0].style['box-shadow']='0px 12px 0px -10px rgb(255, 255, 255), 0px 0px 0px 0px #FFFFFF, 0px 5px 10px -10px #ffffff, 0px 5px 10px -10px #FFFFFF,  0px 2px  #020405, 2px 2px 4px 2px rgba(255, 5, 5, 0)';
       answered[0].style.color='#b9b9b9';
-      answered[0].style['box-shadow']='0px 30px 0px -10px rgb(255, 255, 255), 0px 0px 0px 0px #FFFFFF, 0px 5px 10px -10px #ffffff, 0px 5px 10px -10px #FFFFFF,  0px 2px  #f3f3f3, 2px 2px 4px 2px rgba(255, 5, 5, 0)';
+      answered[0].style['box-shadow']='0px 12px 0px -10px rgb(255, 255, 255), 0px 0px 0px 0px #FFFFFF, 0px 5px 10px -10px #ffffff, 0px 5px 10px -10px #FFFFFF,  0px 2px  #f3f3f3, 2px 2px 4px 2px rgba(255, 5, 5, 0)';
       
     }
     else if (e.target.className === 'answered') {
       answered[0].style.color='black';
-      answered[0].style['box-shadow']='0px 30px 0px -10px rgb(255, 255, 255), 0px 0px 0px 0px #FFFFFF, 0px 5px 10px -10px #ffffff, 0px 5px 10px -10px #FFFFFF,  0px 2px  #020405, 2px 2px 4px 2px rgba(255, 5, 5, 0)';
+      answered[0].style['box-shadow']='0px 12px 0px -10px rgb(255, 255, 255), 0px 0px 0px 0px #FFFFFF, 0px 5px 10px -10px #ffffff, 0px 5px 10px -10px #FFFFFF,  0px 2px  #020405, 2px 2px 4px 2px rgba(255, 5, 5, 0)';
       unanswered[0].style.color='#b9b9b9';
-      unanswered[0].style['box-shadow']='0px 30px 0px -10px rgb(255, 255, 255), 0px 0px 0px 0px #FFFFFF, 0px 5px 10px -10px #ffffff, 0px 5px 10px -10px #FFFFFF,  0px 2px  #f3f3f3, 2px 2px 4px 2px rgba(255, 5, 5, 0)';
+      unanswered[0].style['box-shadow']='0px 12px 0px -10px rgb(255, 255, 255), 0px 0px 0px 0px #FFFFFF, 0px 5px 10px -10px #ffffff, 0px 5px 10px -10px #FFFFFF,  0px 2px  #f3f3f3, 2px 2px 4px 2px rgba(255, 5, 5, 0)';
       
     }
 
   }
+  
+  console.log('Questions ' , questions, '\n', 'Users: ', users);
   
 
 
   return (
     
     <div className='question-table'>
+      <div className='button-container'>
         <div onClick={(event) => QTabChange(event)} className='unanswered'>
           Unanswered Questions
         </div>
@@ -35,18 +45,43 @@ export default function dashboard () {
         <div onClick={(event) => QTabChange(event)} className='answered'>
             Answered Questions
         </div>
-
-        <div className='questions'>
-            <div className='question'>
-
-                
-            </div>
-      <div>
-
       </div>
 
-
+      <div className='question-card'>
+        <div className='who'>
+          <p>  {users.name} asks:</p>
         </div>
+        
+        <img />     
+          
+        <div className='question'>
+        
+        </div>
+      </div>
+
+      <div className='question-card'>
+        <div className='who'>
+          <p>  {users.name} asks:</p>
+        </div>
+        
+        <img />     
+          
+        <div className='question'>
+        
+        </div>
+      </div>
+
+      <div className='question-card'>
+        <div className='who'>
+          <p>  {users.name} asks:</p>
+        </div>
+        
+        <img />     
+          
+        <div className='question'>
+        
+        </div>
+      </div>
     </div>
 
 
