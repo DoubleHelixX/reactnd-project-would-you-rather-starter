428a0fcaec82303211fbf7483344e31dd262be41
DoubleHelix <tjrnyc5@gmail.com>
2021-01-04 17:16:33 -0500

    added more animations to make the app feel smooth

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -36,29 +36,71 @@ export default function dashboard () {
   
   console.log('Questions ' , questions, '\n', 'Users: ', users);
   function viewQuestion(e,i){
-    let hidePreview= document.getElementById(`question-${i}`);
+
+    let questionHeaders = document.getElementsByClassName(`p-title`);
+    let userImgs = document.getElementsByClassName(`question-card-img`);
+    let questions = document.getElementsByClassName(`question`);
+    let questionCards = document.getElementsByClassName(`question-card`);
+    let showQuestions = document.getElementsByClassName(`p-question`);
+    let hidePreviews = document.getElementsByClassName(`answer-form`);
+    let showViewBtns= document.getElementsByClassName(`p-question-btn`);
+
+
+    Object.keys(questionCards).map((i) => {
+      userImgs[i].style['animation'] = 'img-shrink 1.5s ease';
+      userImgs[i].style['width'] = '115px';
+      userImgs[i].style['height'] = '115px';
+      userImgs[i].style['border'] = 'solid 2px #b9b9b9'; 
+      questions[i].style['width']= 'calc(532px - 168px)';
+
+      questionCards[i].style['border'] = 'solid 2px #f3f3f3'; 
+
+      hidePreviews[i].style['animation'] = 'fadeout 1s'; 
+      hidePreviews[i].style['display'] = 'none'; 
+      showQuestions[i].style['animation'] = 'fadein 1s'; 
+      showViewBtns[i].style['animation'] = 'fadein 2s'; 
+      showViewBtns[i].style['display'] = 'block'; 
+      showQuestions[i].style['display'] = 'block'; 
+
+
+    });
+
+   
+
+
+    let hidePreview= document.getElementById(`p-question-${i}`);
     let showQuestion= document.getElementById(`form-${i}`);
     let hideViewBtn= document.getElementById(`viewBtn-${i}`);
+    let question = document.getElementById(`question-${i}`);
     let questionHeader = document.getElementById(`p-title-${i}`);
     let userImg = document.getElementById(`question-card-img-${i}`);
+    let questionCard = document.getElementById(`question-card-${i}`);
 
 
-      userImg.style['width'] = '120px';
-      userImg.style['height'] = '120px';
+
+
+      question.style['width']= 'calc(532px - 188px)';
+
+      questionCard.style['border'] = 'solid 2px #1C6EA4'; 
+
+      userImg.style['animation'] = 'img-growth 1.5s ease';
+      userImg.style['width'] = '130px';
+      userImg.style['height'] = '130px';
+      userImg.style['border'] = 'solid 2px #1C6EA4';
 
     
       hidePreview.style['animation'] = 'fadeOut 1s';
       hideViewBtn.style['animation'] = 'fadeOut 1s';
       hidePreview.style.display='none';
       hideViewBtn.style.display='none';
+
       questionHeader.style['animation'] = 'q-textgrowth 1s';
       questionHeader.style['font-size'] = '24px';
       questionHeader.style['padding-bottom'] = '5px';
-      questionHeader.style['padding-left'] = '85px';
+      questionHeader.style['padding-left'] = '55px';
 
       showQuestion.style['animation'] = 'fadein 1s';
       showQuestion.style.display='inline-block';
-      console.log('asdasas', hidePreview.style['animation'])
 
 
 
@@ -91,38 +133,25 @@ export default function dashboard () {
       </div>
       {[...Array(20)].map((x, i) =>(
       
-      <div className='question-card'>
+      <div className='question-card' id = {`question-card-${i}`} >
         <div className='who'>
           <p>  {users.name} asks:</p>
         </div>
         
         <div className='question-card-body'>
-          <img src={users.avatarURL } alt="Portfolio" id ={`question-card-img-${i}`} />  
+          <img className ='question-card-img' src={users.avatarURL } alt="Portfolio" id ={`question-card-img-${i}`} />  
 
           <div className='side-line-break'>
             
             </div>  
         
-          <div className='question'>
+          <div className='question' id ={`question-${i}`}>
             <p className='p-title' id ={`p-title-${i}`}> Would You Rather </p>
-            <p className='p-question' id ={`question-${i}`}> ...{questions.optionOne.text.substring(0,15)}...</p>
+            <p className='p-question' id ={`p-question-${i}`}> ...{questions.optionOne.text.substring(0,15)}...</p>
             <button id ={`viewBtn-${i}`} onClick= {(event) => viewQuestion(event, i)} className='p-question-btn'> View Full </button>
              
             <form id ={`form-${i}`} class='answer-form'>
 
-{/* 
-                  <input type = "radio"
-                        id = "optionOne"
-                        checked = "checked" />
-                  <label for = "sizeSmall">{questions.optionOne.text}</label>
-                  <br/>
-                  <input type = "radio"
-                        id = "optionTwo"
-                        />
-                  <label for = "sizeMed">{questions.optionTwo.text}</label>
-                  <br/>
-                  
-                  <input className='submit-answer' type="submit" value="Submit"/>  */}
                 <fieldset>
 
                   <label for="def"><input  id = "optionOne" type="radio" name="group" value="one" />
